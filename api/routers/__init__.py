fda6422e774169034c301933f8736ea7912e08b1
Lucas Bernardes <lucascbernardes@live.com>
2022-09-10 12:44:37 -0300

    adiciona testes unitarios

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -5,11 +5,13 @@ from api.routers import cart
 from api.routers import order
 from api.routers import product
 from api.routers import token
+from api.routers import healthcheck
 from api.routers.admin import product as product_adm
 from api.routers.admin import user as user_adm
 
 api_router = APIRouter()
 
+api_router.include_router(healthcheck.router)
 api_router.include_router(account.router)
 api_router.include_router(address.router)
 api_router.include_router(cart.router)
